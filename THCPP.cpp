d2b2591159a21295977991850b66c4958dc9f0ca
anhkhoa289 <anhkhoa2890@gmail.com>
2017-09-24 22:18:16 +0700

    Learning C++

--- THCPP.cpp
+++ THCPP.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+// Nếu đã khai báo rồi thì sẽ không cần phải dùng std:: nữa
+// ex: std::cout -> cout
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World";
+    cout << "Hello World" << endl;
+    cout << "Hello Again" << endl;
+    cout << "HUHU" << endl;
+
+    int a = 5;
+    int b = 6;
+    int c = a&b;
+    int d = b&a;
+    cout << c << endl << d << endl;
+
     return 1;
 }
